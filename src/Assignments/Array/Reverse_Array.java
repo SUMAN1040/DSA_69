2b4927c66ec8e72876c0a85f135ed2262bad91b9
Innox1040 <uemj2003@gmail.com>
2026-01-02 12:18:10 +0530

    update all

--- src/Assignments/Array/Reverse_Array.java
+++ src/Assignments/Array/Reverse_Array.java
@@ -12,16 +12,31 @@ public class Reverse_Array {
             arr[i] = in.nextInt();
         }
         reverse(arr);
+
+        // Print reversed array
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
         in.close();
     }
 
     static void reverse(int[] arr) {
-        int[] reverse = new int[arr.length];
-        for (int i = 0; i < arr.length; i++) {
-            reverse[i] = arr[arr.length - 1 - i];
-        }
-        for (int i = 0; i < arr.length; i++) {
-            System.out.println(reverse[i]);
+//        int[] reverse = new int[arr.length];
+//        for (int i = 0; i < arr.length; i++) {
+//            reverse[i] = arr[arr.length - 1 - i];
+//        }
+//        for (int i = 0; i < arr.length; i++) {
+//            System.out.println(reverse[i]);
+//        }
+
+
+        int start = 0, end = arr.length - 1;
+        while (start < end) {
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
         }
     }
 }
