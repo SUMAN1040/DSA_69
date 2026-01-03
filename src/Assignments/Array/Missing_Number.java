b8b3ce3af66e0e6e3e197e211c47711f0d35a785
Innox1040 <uemj2003@gmail.com>
2026-01-03 14:11:11 +0530

    update all

--- src/Assignments/Array/Missing_Number.java
+++ src/Assignments/Array/Missing_Number.java
@@ -4,20 +4,36 @@ package Assignments.Array;
 
 public class Missing_Number {
 
-
     public static void main(String[] args) {
+
+        //Using mathematical formula
+//        int[] arr = {1, 2, 3, 5};
+//        int n = arr.length + 1;
+//
+//        int expectedSum = n * (n + 1) / 2;
+//        int actualSum = 0;
+//
+//        for (int num : arr) {
+//            actualSum += num;
+//        }
+//
+//        int missingSum = expectedSum - actualSum;
+//        System.out.println("Missing Number: " + missingSum);
+
+
+        //Using XOR Method
         int[] arr = {1, 2, 3, 5};
         int n = arr.length + 1;
 
-        int expectedSum = n * (n + 1) / 2;
-        int actualSum = 0;
+        int xor = 0;
 
-        for (int num : arr) {
-            actualSum += num;
+        for (int i = 1; i <= n; i++) {
+            xor ^= i;
         }
-
-        int missingSum = expectedSum - actualSum;
-        System.out.println("Missing Number: " + missingSum);
+        for (int num : arr) {
+            xor ^= num;
+        }
+        System.out.println("Missing Number: " + xor);
     }
 
 }
