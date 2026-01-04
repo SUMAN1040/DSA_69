a8955c9e20b31a0eb2a817c7af7a6388ed245c38
Innox1040 <uemj2003@gmail.com>
2026-01-04 11:39:42 +0530

    update all

---
+++ src/Assignments/Array/Rotate_Element.java
@@ -0,0 +1,37 @@
+//Given an array of integers and an integer k, rotate the array by k positions.
+package Assignments.Array;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Rotate_Element {
+    public static void main(String[] args) {
+//        Scanner in = new Scanner(System.in);
+//        int n = in.nextInt();
+//        int[] arr = new int[n];
+//
+//        for (int i = 0; i < n; i++) {
+//            arr[i] = in.nextInt();
+//        }
+//        System.out.println(Arrays.toString(arr));
+//
+//
+//        int k = in.nextInt();
+//        k %= n;
+//
+//        int[] result = new int[n];
+//        int index = 0;
+//
+//        for (int i = n - k; i < n; i++) {
+//            result[index++] = arr[i];
+//        }
+//
+//        for (int i = 0; i < n - k; i++) {
+//            result[index++] = arr[i];
+//        }
+//
+//        System.out.println(Arrays.toString(result));
+//        in.close();
+
+    }
+}
