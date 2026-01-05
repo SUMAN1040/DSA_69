cb7b4967b7b1835d06345852067a5a37fb86edd3
Innox1040 <uemj2003@gmail.com>
2026-01-05 10:15:11 +0530

    update all

---
+++ src/Assignments/Array/Remove_Duplicates.java
@@ -0,0 +1,34 @@
+//You are given a sorted array. Remove the duplicates in-place such that each element appears only once and return the new length of the array.
+package Assignments.Array;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Remove_Duplicates {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int[] arr = new int[n];
+
+        int newLength = removeDuplicates(arr);
+        System.out.println("The new length is " + newLength);
+        System.out.println("Array after removing duplicates is: ");
+        for (int i = 0; i < newLength; i++) {
+            System.out.println(arr[i] + " ");
+        }
+    }
+
+    public static int removeDuplicates(int[] arr) {
+        if (arr.length == 0) {
+            return 0;
+        }
+        int i = 0;
+        for (int j = 0; j < arr.length; j++) {
+            if (arr[j] != arr[i]) {
+                i++;
+                arr[i] = arr[j];
+            }
+        }
+        return i + 1;
+    }
+}
--- src/Assignments/Array/Rotate_Element.java
+++ src/Assignments/Array/Rotate_Element.java
@@ -7,21 +7,20 @@ import java.util.Scanner;
 public class Rotate_Element {
     public static void main(String[] args) {
 
-        //Using suffix base rotation, Right Rotate(The complexity of this time: O(n) and the space: O(n)
 
-//        Scanner in = new Scanner(System.in);
-//        int n = in.nextInt();
-//        int[] arr = new int[n];
-//
-//        for (int i = 0; i < n; i++) {
-//            arr[i] = in.nextInt();
-//        }
-//        System.out.println(Arrays.toString(arr));
-//
-//
-//        int k = in.nextInt();
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = in.nextInt();
+        }
+        System.out.println(Arrays.toString(arr));
+
+        int k = in.nextInt();
+
+        //Using suffix base rotation, Right Rotate(The complexity of this time: O(n) and the space: O(n)
 //        k %= n;
-//
 //        int[] result = new int[n];
 //        int index = 0;
 //
@@ -36,5 +35,61 @@ public class Rotate_Element {
 //        System.out.println(Arrays.toString(result));
 //        in.close();
 
+
+        //Left Rotation by K Positions:  (Time complexity: O(n) & Space Complexity: O(1)
+//        leftRotate(arr, k);
+//        for (int num : arr) {
+//            System.out.print(num + " ");
+//        }
+//        in.close();
+
+
+        //Right Rotation
+        rightRotate(arr, k);
+        System.out.println("Array after right rotation:");
+        System.out.println(Arrays.toString(arr));
+        in.close();
+
     }
+
+    //Left Rotation by K Positions
+//    public static void leftRotate(int[] arr, int k) {
+//        int n = arr.length;
+//        k = k % n;
+//        reverse(arr, 0, k - 1);
+//        reverse(arr, k, n - 1);
+//        reverse(arr, 0, n - 1);
+//    }
+//
+//    public static void reverse(int[] arr, int start, int end) {
+//        while (start < end) {
+//            int temp = arr[start];
+//            arr[start] = arr[end];
+//            arr[end] = temp;
+//            start++;
+//            end--;
+//        }
+//    }
+
+
+    //Right rotation
+    public static void rightRotate(int[] arr, int k) {
+        int n = arr.length;
+        k = k % n;
+
+        reverse(arr, 0, n - 1);
+        reverse(arr, 0, k - 1);
+        reverse(arr, k, n - 1);
+    }
+
+    private static void reverse(int[] arr, int start, int end) {
+        while (start < end) {
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
+        }
+    }
+
 }
