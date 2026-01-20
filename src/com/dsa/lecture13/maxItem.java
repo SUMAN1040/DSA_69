2e2afa99d86179d8404084000c26d9e861cf9156
Innox1040 <uemj2003@gmail.com>
2026-01-20 11:06:11 +0530

    update all

--- src/com/dsa/lecture13/maxItem.java
+++ src/com/dsa/lecture13/maxItem.java
@@ -1,4 +1,27 @@
 package com.dsa.lecture13;
 
+import java.util.Arrays;
+
 public class maxItem {
+    public static void main(String[] args) {
+        int[][] arr = {
+                {23, 4, 1},
+                {12, 14, 15, 5},
+                {20, 21, 11, 45},
+                {18, 12}
+         };
+        System.out.println(max(arr));
+    }
+
+    static int max(int[][] arr) {
+        int max = Integer.MIN_VALUE;
+        for (int[] ints : arr) {
+            for (int anInt : ints) {
+                if (anInt > max) {
+                    max = anInt;
+                }
+            }
+        }
+        return max;
+    }
 }
