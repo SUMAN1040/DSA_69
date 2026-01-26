5a87fb7d6485d3f64f494e944c80d2a1336864ec
Innox1040 <uemj2003@gmail.com>
2026-01-26 10:18:05 +0530

    update all

---
+++ src/com/dsa/lecture15/Celling.java
@@ -0,0 +1,39 @@
+package com.dsa.lecture15;
+
+import java.util.Scanner;
+
+public class Celling {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter the number of elements in the array: ");
+        int n = in.nextInt();
+        int[] arr = new int[n];
+        System.out.println("Enter the elements in the array: ");
+        for (int i = 0; i < n; i++) {
+            arr[i] = in.nextInt();
+        }
+
+        System.out.println("Enter the target element: ");
+        int target = in.nextInt();
+
+        System.out.println(celling(arr, target));
+
+    }
+
+    static int celling(int[] arr, int target) {
+        int start = 0;
+        int end = arr.length - 1;
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (target < arr[mid]) {
+                end = mid - 1;
+            } else if (target > arr[mid]) {
+                start = mid + 1;
+            } else {
+                return mid;
+            }
+        }
+        return start;
+    }
+}
