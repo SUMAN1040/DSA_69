9050f024fa059bb62befe3ac0398fe39e0d979f0
Innox1040 <uemj2003@gmail.com>
2026-03-03 12:20:33 +0530

    update all

---
+++ src/com/dsa/lecture20/Main.java
@@ -0,0 +1,29 @@
+package com.dsa.lecture20;
+
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int[] arr = {3, 5, 2, 1, 4};
+        sort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+    static void sort(int[] arr){
+        int i = 0;
+        while(i < arr.length){
+            int correctIndex = arr[i] - 1;
+            if(arr[i] != arr[correctIndex]){
+                swap(arr, i, correctIndex);
+            }else{
+                i++;
+            }
+        }
+    }
+
+    static void swap(int[] arr, int start, int end){
+        int temp = arr[start];
+        arr[start] = arr[end];
+        arr[end] = temp;
+    }
+}
