c8a42b0674e08d1d0b8a0592afca6024343f2586
Innox1040 <uemj2003@gmail.com>
2026-03-07 13:25:50 +0530

    update all

---
+++ src/com/dsa/lecture21/Main.java
@@ -0,0 +1,16 @@
+package com.dsa.lecture21;
+
+public class  Main {
+    public static void main(String[] args) {
+        int a = 10;
+        int[] arr = {2, 3, 4};
+        String name = "Suman Kumar Dey";
+
+        String a1 = "Suman";
+        System.out.println(a1);
+        a1 = "Dey";
+        System.out.println(a1);
+
+
+    }
+}
---
+++ src/com/dsa/lecture22/Main.java
@@ -0,0 +1,77 @@
+package com.dsa.lecture22;
+
+public class Main {
+    public static void main(String[] args) {
+        pattern1(4);
+        System.out.println();
+        pattern2(4);
+        System.out.println();
+        pattern3(4);
+        System.out.println();
+        pattern4(4);
+        System.out.println();
+        pattern5(4);
+        System.out.println();
+        pattern6(4);
+
+    }
+
+    static void pattern1(int n) {
+        for (int row = 1; row <= n; row++) {
+            for (int col = 1; col <= n; col++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    static void pattern2(int n) {
+        for (int row = 1; row <= n; row++) {
+            for (int col = 1; col <= row; col++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+
+    static void pattern3(int n) {
+        for (int row = n; row >= 0; row--) {
+            for (int col = row; col >= 1; col--) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+
+    static void pattern4(int n) {
+        for (int row = 1; row <= n; row++) {
+            for (int col = 1; col <= row; col++) {
+                System.out.print(col);
+            }
+            System.out.println();
+        }
+    }
+
+    static void pattern5(int n) {
+        for (int row = 0; row < 2 * n; row++) {
+            int totalColsInRow = row > n ? 2 * n - row : row;
+            for (int col = 0; col < totalColsInRow; col++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+
+    static void pattern6(int n) {
+        for (int row = 0; row < n; row++) {
+            for (int col = 0; col < n; col++) {
+                if (col < n - row - 1) {
+                    System.out.print(" ");
+                } else {
+                    System.out.print("*");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
