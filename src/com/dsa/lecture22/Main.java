9276fda87de57bf709af0697c0e39e7406cd4f9e
Innox1040 <uemj2003@gmail.com>
2026-03-10 12:46:33 +0530

    update all

--- src/com/dsa/lecture22/Main.java
+++ src/com/dsa/lecture22/Main.java
@@ -2,20 +2,12 @@ package com.dsa.lecture22;
 
 public class Main {
     public static void main(String[] args) {
-        pattern1(4);
-        System.out.println();
-        pattern2(4);
-        System.out.println();
-        pattern3(4);
-        System.out.println();
-        pattern4(4);
-        System.out.println();
-        pattern5(4);
-        System.out.println();
-        pattern6(4);
+        pattern31(4);
 
     }
 
+
+    //pattern 1
     static void pattern1(int n) {
         for (int row = 1; row <= n; row++) {
             for (int col = 1; col <= n; col++) {
@@ -25,6 +17,7 @@ public class Main {
         }
     }
 
+    //pattern 2
     static void pattern2(int n) {
         for (int row = 1; row <= n; row++) {
             for (int col = 1; col <= row; col++) {
@@ -34,24 +27,29 @@ public class Main {
         }
     }
 
+    //pattern 3
     static void pattern3(int n) {
-        for (int row = n; row >= 0; row--) {
-            for (int col = row; col >= 1; col--) {
+        for (int row = 1; row <= n; row++) {
+            for (int col = 1; col <= n - row + 1; col++) {
                 System.out.print("* ");
             }
             System.out.println();
         }
     }
 
+
+    //pattern 4
     static void pattern4(int n) {
         for (int row = 1; row <= n; row++) {
             for (int col = 1; col <= row; col++) {
-                System.out.print(col);
+                System.out.print(col + " ");
             }
             System.out.println();
         }
     }
 
+
+    //pattern 5
     static void pattern5(int n) {
         for (int row = 0; row < 2 * n; row++) {
             int totalColsInRow = row > n ? 2 * n - row : row;
@@ -62,6 +60,8 @@ public class Main {
         }
     }
 
+
+    //pattern 6
     static void pattern6(int n) {
         for (int row = 0; row < n; row++) {
             for (int col = 0; col < n; col++) {
@@ -70,6 +70,71 @@ public class Main {
                 } else {
                     System.out.print("*");
                 }
+            }
+            System.out.println();
+        }
+    }
+
+    //pattern 17
+    static void pattern17(int n) {
+        for (int row = 1; row <= 2 * n; row++) {
+            int c = row > n ? 2 * n - row : row;
+
+            for (int space = 0; space < n - c; space++) {
+                System.out.print("  ");
+            }
+            for (int col = c; col >= 1; col--) {
+                System.out.print(col + " ");
+            }
+            for (int col = 2; col <= c; col++) {
+                System.out.print(col + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    //pattern 28
+    static void pattern28(int n) {
+        for (int row = 0; row < 2 * n; row++) {
+
+            int totalColsInRow = row > n ? 2 * n - row : row;
+            int spaceCount = n - totalColsInRow;
+
+            for (int s = 0; s < spaceCount; s++) {
+                System.out.print(" ");
+            }
+            for (int col = 0; col < totalColsInRow; col++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+
+
+    //pattern 30
+    static void pattern30(int n) {
+        for (int row = 1; row <= n; row++) {
+            for (int space = 0; space < n - row; space++) {
+                System.out.print(" ");
+            }
+            for (int col = row; col >= 1; col--) {
+                System.out.print(col);
+            }
+            for (int col = 2; col <= row; col++) {
+                System.out.print(col);
+            }
+            System.out.println();
+        }
+    }
+
+    //pattern 31
+    static void pattern31(int n) {
+        int org = n;
+        n = 2 * n;
+        for (int row = 0; row <= n; row++) {
+            for(int col = 0; col <= n; col++) {
+                int atIndex = org - Math.min(Math.min(row, col), Math.min(n - row, n - col));
+                System.out.print(atIndex + " ");
             }
             System.out.println();
         }
