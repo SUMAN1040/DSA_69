ae5f6fea5e8d538787171c2cd7139f83afef9e7e
Innox1040 <uemj2003@gmail.com>
2026-03-22 11:00:10 +0530

    update all

---
+++ src/com/dsa/lecture25/PowOfTwo.java
@@ -0,0 +1,10 @@
+package com.dsa.lecture25;
+
+public class PowOfTwo {
+    public static void main(String[] args) {
+        int n = 0; // note: fix for n = 0
+
+        boolean ans = (n & (n - 1)) == 0;
+        System.out.println(ans);
+    }
+}
