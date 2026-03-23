6728cc1be1b6de1ebc69a6e10970689b691cf754
Innox1040 <uemj2003@gmail.com>
2026-03-23 12:02:22 +0530

    update all

---
+++ src/com/dsa/lecture26/Seive.java
@@ -0,0 +1,26 @@
+package com.dsa.lecture26;
+
+
+public class Seive {
+    public static void main(String[] args) {
+        int n = 40;
+        boolean[] primes = new boolean[n + 1];
+        siv(n, primes);
+    }
+
+    //false in array means number in prime
+    static void siv(int n, boolean[] primes) {
+        for (int i = 2; i * i <= n; i++) {
+            if (!primes[i]) {
+                for (int j = i * 2; j <= n; j += i) {
+                    primes[j] = true;
+                }
+            }
+        }
+        for (int i = 2; i <= n; i++) {
+            if (!primes[i]) {
+                System.out.print(i + " ");
+            }
+        }
+    }
+}
