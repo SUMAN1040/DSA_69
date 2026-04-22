98957c0addaeffe5b946205f143eb3d8e3ad5f90
SUMAN1040 <uemj2003@gmail.com>
2026-04-22 12:27:24 +0530

    update all

--- src/com/dsa/lecture32/SubSet.java
+++ src/com/dsa/lecture32/SubSet.java
@@ -1,12 +1,13 @@
 package com.dsa.lecture32;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class SubSet {
     public static void main(String[] args) {
         int[] arr = {1, 2, 3};
-        List<List<Integer>> ans = subset(arr);
+        List<List<Integer>> ans = subSetDuplicate(arr);
         for (List<Integer> list : ans) {
             System.out.println(list);
         }
@@ -22,6 +23,31 @@ public class SubSet {
             for (int i = 0; i < n; i++) {
                 List<Integer> internal = new ArrayList<>(outer.get(i));
                 internal.add(num);
+                outer.add(internal);
+            }
+        }
+        return outer;
+    }
+
+
+    static List<List<Integer>> subSetDuplicate(int[] arr) {
+        Arrays.sort(arr);
+        List<List<Integer>> outer = new ArrayList<>();
+        outer.add(new ArrayList<>());
+
+        int start = 0;
+        int end = 0;
+        for (int i = 0; i < arr.length; i++) {
+            start = 0;
+            //if my current and previous element is same, s = e + 1
+            if (i > 0 && arr[i] == arr[i - 1]) {
+                start = end + 1;
+            }
+            end = outer.size() - 1;
+            int n = outer.size();
+            for (int j = start; j < n; j++) {
+                List<Integer> internal = new ArrayList<>(outer.get(j));
+                internal.add(arr[i]);
                 outer.add(internal);
             }
         }
