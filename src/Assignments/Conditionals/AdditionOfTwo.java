b854d191897671cc36da425bc77dbc82ba226eb2
SUMAN1040 <uemj2003@gmail.com>
2026-05-25 23:13:27 +0530

    update all

--- src/Assignments/Conditionals/AdditionOfTwo.java
+++ src/Assignments/Conditionals/AdditionOfTwo.java
@@ -7,7 +7,6 @@ public class AdditionOfTwo {
         Scanner in = new Scanner(System.in);
         int num1 = in.nextInt();
         int num2 = in.nextInt();
-
         System.out.println(num1 + num2);
     }
 }
