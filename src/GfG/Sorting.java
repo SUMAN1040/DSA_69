9e910d81ecb22d08c7e55d5a07ced45d6bbb7552
SUMAN1040 <uemj2003@gmail.com>
2026-06-11 23:41:00 +0530

    update all

--- src/GfG/Sorting.java
+++ src/GfG/Sorting.java
@@ -9,13 +9,11 @@ public class Sorting {
             System.out.print(arr[i] + " ");
     }
 }
-
 static void insertionSort(int arr[]) {
     int n = arr.length;
     for (int i = 1; i < n; ++i) {
         int key = arr[i];
         int j = i - 1;
-
             /* Move elements of arr[0..i-1], that are
                greater than key, to one position ahead
                of their current position */
