5d7ab354a2adfe4b38f66bb1cb97be1d1e503847
SUMAN1040 <uemj2003@gmail.com>
2026-06-12 23:16:45 +0530

    update all

--- src/GfG/StockBuySell.java
+++ src/GfG/StockBuySell.java
@@ -5,12 +5,10 @@ public class StockBuySell {
         int[] prices = {7, 10, 1, 3, 6, 9, 2};
         int minPrice = prices[0];
         int maxProfit = 0;
-
         for(int i = 1; i < prices.length; i++){
             maxProfit = Math.max(maxProfit, prices[i] - minPrice);
             minPrice = Math.min(minPrice, prices[i]);
         }
-
         System.out.println(maxProfit);
     }
 }
